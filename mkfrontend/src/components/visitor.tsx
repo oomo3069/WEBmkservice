9af20aa4417e2a6f430066945009b25198b23756
oomo3069 <oomo1212312121@gmail.com>
2025-06-09 22:50:07 +0700

    hello guy

--- mkfrontend/src/components/visitor.tsx
+++ mkfrontend/src/components/visitor.tsx
@@ -21,7 +21,7 @@ const VisitorCounter = () => {
   return (
     
       <div className="bg-v">
-        <div style={{ textAlign: "center", fontSize: "14px", marginTop: "0" }}>
+        <div style={{ textAlign: "center", fontSize: "12px", marginTop: "0" }}>
           มีผู้เข้าชมทั้งหมด: <strong>{count}</strong> คน
         </div>
       </div>
