129a7e921a66d74c884b466db17023a7b35f4be4
oomo3069 <oomo1212312121@gmail.com>
2026-03-18 14:20:53 +0700

    deploy bankend

--- mkbackend/src/config/db.ts
+++ mkbackend/src/config/db.ts
@@ -1,15 +1,22 @@
-import { Pool } from 'pg';
+import { Connection, Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: Number(process.env.DB_PORT),
+//   ssl: false // สำคัญ: local ไม่ต้องใช้ ssl
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-  ssl: false // สำคัญ: local ไม่ต้องใช้ ssl
+  connectionString: process.env.DATABASE_URL,
+  ssl:{
+    rejectUnauthorized:false
+  }
 });
 
 pool.connect()
